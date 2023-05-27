29472d1c22f8daa90b9313f20d9210bb8a3a7690
minjoo <minjoo0729@kaist.ac.kr>
2023-05-27 14:20:22 +0900

    Find extra message and deleted

--- src/pages/answer.js
+++ src/pages/answer.js
@@ -69,16 +69,7 @@ const Answer = () => {
 
   return (
     <div id="load">
-        <h1>Your result: {modifiedIE} {modifiedNS} {modifiedFT} {modifiedPJ}</h1>
-        <h2>result : {IE} {NS} {FT} {PJ} </h2>
         <Result result={result} mbti = {mbti}/>
-        <div>G</div>
-        <div>N</div>
-        <div>I</div>
-        <div>D</div>
-        <div>A</div>
-        <div>O</div>
-        <div>L</div>
     </div>
   );
 };
